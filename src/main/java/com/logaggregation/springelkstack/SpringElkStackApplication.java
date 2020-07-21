ed260f10f29060cdda3cb65aa4c1de09e15c0cf5
samantrs <https://samantrs@git.rabobank.nl>
2020-07-21 20:30:25 +0200

    first commit

---
+++ src/main/java/com/logaggregation/springelkstack/SpringElkStackApplication.java
@@ -0,0 +1,13 @@
+package com.logaggregation.springelkstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElkStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElkStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logaggregation/springelkstack/SpringElkStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logaggregation.springelkstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElkStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
